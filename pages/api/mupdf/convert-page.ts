411466c0feadfd57a4fba6cc79e1bc8130062dd4
scgreenhalgh <me@seangreenhalgh.com>
2025-04-07 12:05:35 +0800

    Revert "fix: update maxDuration for convert-page function to 60 seconds"
    This reverts commit 7bb3ff08dba4e49aa248a13ec94b8fa81862bc66.

--- pages/api/mupdf/convert-page.ts
+++ pages/api/mupdf/convert-page.ts
@@ -7,9 +7,9 @@ import { putFileServer } from "@/lib/files/put-file-server";
 import prisma from "@/lib/prisma";
 import { log } from "@/lib/utils";
 
-// This function can run for a maximum of 60 seconds
+// This function can run for a maximum of 120 seconds
 export const config = {
-  maxDuration: 60,
+  maxDuration: 180,
 };
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
